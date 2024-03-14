399b241df08657b81d1627f4a962415d7dd9d4fe
JoseCibie <163325191+JoseCibie@users.noreply.github.com>
2024-03-14 18:07:52 +0000

    listo el cachipun

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -11,25 +11,25 @@ def cachipun():
     person=input("piedra, papel o tijera?")
     if person=="piedra":
         if comp==person:
-            fin="Empate"
+            fin="Compu eligio piedra y hubo un Empate"
         if comp=="papel":
-            fin="Compu gana"
+            fin="Compu eligio papel y Compu gana"
         else:
-            fin="Usuario gana"
+            fin="Compu eligio tijera y Usuario gana"
     if person=="papel":
         if person==comp:
-            fin="Empate"
+            fin="Compu eligio papel y hubo un Empate"
         if comp=="piedra":
-            fin="Usuario gana"
+            fin="Compu eligio piedra y Usuario gana"
         else:
-            fin="Compu gana"
+            fin="Compu eligio tijera y Compu gana"
     if person=="tijera":
         if person==comp:
-            fin="Empate"
+            fin="Compu eligio tijera y hubo un Empate"
         if comp=="papel":
-            fin="Usuario gana"
+            fin="Compu eligio papel y Usuario gana"
         else:
-            fin="Compu gana"
+            fin="Compu eligio piedra y Compu gana"
     print(fin)
     return
     pass
