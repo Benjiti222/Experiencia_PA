4379a06c821103757a37dba9e0e4a817edadb353
FelipeMarinNegrete <163420412+FelipeMarinNegrete@users.noreply.github.com>
2024-03-14 14:23:42 +0000

    coment

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,4 +1,42 @@
+import random
+
 def adivinar_par_o_impar():
+    x = input("Par o impar: ")
+    N = random.randint(1, 10)  # Genera un número entre 1 y 10
+    y = str(input("¿Estás seguro?"))
+    if y=="si":
+        print("checkpoint1")
+        if x.lower() == "par":
+            print("checkpoint2")
+            if N % 2 == 0:
+                R = "Correcto :)"
+            else:
+                R = "Incorrecto :("
+        elif x.lower() == "impar":
+            if N % 2 != 0:
+                R = "Correcto :)"
+            else:
+                R = "Incorrecto :("
+        print(R)
+    elif y=="no":
+        x = input("Entonces, ¿par o impar?: ")
+        if x.lower() == "par":
+            if N % 2 == 0:
+                R = "Correcto :)"
+            else:
+                R = "Incorrecto :("
+        elif x.lower() == "impar":
+            if N % 2 != 0:
+                R = "Correcto :)"
+            else:
+                R = "Incorrecto :("
+        print(R)
+    return R
+
+
+            
+
+
     """
     Esta función representa el juego de adivinar si un número es par o impar.
     Tienes que permitir que el usuario ingrese una de las dos opciones y
